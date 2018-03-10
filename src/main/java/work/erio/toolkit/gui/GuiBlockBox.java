bbdfc10d4d77c3ceb735c713c8bc2ad96a49d953
Erioifpud <erioifpud@zuo.si>
2018-03-10 22:06:27 +0800

    新增GuiItemSelect控件

--- src/main/java/work/erio/toolkit/gui/GuiBlockBox.java
+++ src/main/java/work/erio/toolkit/gui/GuiBlockBox.java
@@ -4,13 +4,14 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import org.lwjgl.input.Keyboard;
-import scala.Char;
+import work.erio.toolkit.Toolkit;
 import work.erio.toolkit.tile.TileEntityBox;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.function.Function;
 
 /**
  * Created by Erioifpud on 2018/3/8.
@@ -23,19 +24,33 @@ public class GuiBlockBox extends GuiScreen {
 
     private GuiTextField powerTextField;
 
+    private GuiItemSelect stack1Selector;
+    private GuiItemSelect stack16Selector;
+    private GuiItemSelect stack64Selector;
+
     public GuiBlockBox(TileEntityBox tileEntity) {
         this.tileEntity = tileEntity;
         this.power = 0;
+        //initItemSelectors();
     }
 
-    @Override
-    public void updateScreen() {
-        super.updateScreen();
-        this.powerTextField.updateCursorCounter();
+    private void initItemSelectors() {
+        Function<String, String> getTranslationKey = s -> String.format("message.%s.%s", Toolkit.MODID, s);
+        TextComponentTranslation title1 = new TextComponentTranslation(getTranslationKey.apply("stack1"));
+        title1.getStyle().setColor(TextFormatting.GREEN);
+        TextComponentTranslation title16 = new TextComponentTranslation(getTranslationKey.apply("stack16"));
+        title16.getStyle().setColor(TextFormatting.AQUA);
+        TextComponentTranslation title64 = new TextComponentTranslation(getTranslationKey.apply("stack64"));
+        title64.getStyle().setColor(TextFormatting.GOLD);
+        this.stack1Selector = new GuiItemSelect(this, title1, 50, 50, itemStack -> itemStack.getMaxStackSize() == 1);
+        this.stack16Selector = new GuiItemSelect(this, title16, 50, 100, itemStack -> itemStack.getMaxStackSize() == 16);
+        this.stack64Selector = new GuiItemSelect(this, title64, 50, 150, itemStack -> itemStack.getMaxStackSize() == 64);
+
     }
 
     @Override
     public void initGui() {
+        super.initGui();
         Keyboard.enableRepeatEvents(true);
         this.buttonList.clear();
         this.doneButton = this.addButton(new GuiButton(0, this.width / 2 - 4 - 150, 210, 150, 20, I18n.format("gui.done")));
@@ -46,7 +61,24 @@ public class GuiBlockBox extends GuiScreen {
         this.powerTextField.setMaxStringLength(10);
         this.powerTextField.setFocused(true);
         this.powerTextField.setText("0");
+
+        initItemSelectors();
     }
+
+    @Override
+    public void updateScreen() {
+        super.updateScreen();
+        this.powerTextField.updateCursorCounter();
+    }
+
+    @Override
+    public void handleMouseInput() throws IOException {
+        super.handleMouseInput();
+        this.stack1Selector.onWheelChanged();
+        this.stack16Selector.onWheelChanged();
+        this.stack64Selector.onWheelChanged();
+    }
+
 
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
@@ -54,17 +86,20 @@ public class GuiBlockBox extends GuiScreen {
             if (button.id == 1) {
                 this.mc.displayGuiScreen((GuiScreen) null);
             } else if (button.id == 0) {
+                updateTileEntity();
                 this.mc.displayGuiScreen((GuiScreen) null);
             }
         }
     }
 
+    private void updateTileEntity() {
+
+    }
+
     @Override
     protected void keyTyped(char typedChar, int keyCode) throws IOException {
         super.keyTyped(typedChar, keyCode);
-        //TODO checkKeyInput(typedChar, keyCode);
         this.powerTextField.textboxKeyTyped(typedChar, keyCode);
-
 
         if (keyCode != Keyboard.KEY_RETURN && keyCode != Keyboard.KEY_NUMPADENTER) {
             if (keyCode == Keyboard.KEY_ESCAPE) {
@@ -91,6 +126,9 @@ public class GuiBlockBox extends GuiScreen {
         drawDefaultBackground();
         super.drawScreen(mouseX, mouseY, partialTicks);
         this.powerTextField.drawTextBox();
+        this.stack1Selector.draw(mouseX, mouseY);
+        this.stack16Selector.draw(mouseX, mouseY);
+        this.stack64Selector.draw(mouseX, mouseY);
     }
 
     @Override
--- src/main/java/work/erio/toolkit/gui/GuiItemSelect.java
+++ src/main/java/work/erio/toolkit/gui/GuiItemSelect.java
@@ -1,7 +1,118 @@
 package work.erio.toolkit.gui;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.fml.client.config.GuiUtils;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import org.lwjgl.input.Mouse;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 /**
  * Created by Erioifpud on 2018/3/9.
  */
-public class GuiItemSelect {
+public class GuiItemSelect extends Gui {
+    public static final int WIDTH = 18;
+    public static final int HEIGHT = 18;
+    public static final int COLOR_BACKGROUND = 0xFFBDC3C7;
+    public static final int COLOR_FOREGROUND = 0xFFECF0F1;
+    private static Minecraft mc = Minecraft.getMinecraft();
+    private GuiScreen parent;
+    private ITextComponent title;
+    private int x;
+    private int y;
+    private int currentIndex;
+    private List<ItemStack> itemStacks;
+
+    public GuiItemSelect(GuiScreen parent, ITextComponent title, int x, int y, Predicate<ItemStack> filter) {
+        this.itemStacks = ForgeRegistries.ITEMS.getValuesCollection().parallelStream().map(ItemStack::new).filter(filter).collect(Collectors.toList());
+        this.parent = parent;
+        this.title = title;
+        this.x = x;
+        this.y = y;
+        this.currentIndex = 0;
+    }
+
+    public void draw(int mouseX, int mouseY) {
+        drawFrame();
+        drawItems(x, y);
+        drawInfo(mouseX, mouseY);
+    }
+
+    private void drawInfo(int mouseX, int mouseY) {
+        if (isMouseOver()) {
+            String textProgress = String.format("%d/%d", currentIndex + 1, itemStacks.size());
+            GuiUtils.drawHoveringText(Arrays.asList(title.getFormattedText(), textProgress), mouseX, mouseY, parent.width, parent.height, 300, mc.fontRenderer);
+        }
+    }
+
+    public void onWheelChanged() {
+        if (isMouseOver()) {
+            switchItem();
+        }
+    }
+
+    private void switchItem() {
+        int dWheel = Integer.signum(Mouse.getEventDWheel());
+        if (dWheel < 0) {
+            currentIndex -= currentIndex > 0 ? 1 : 0;
+        } else if (dWheel > 0) {
+            currentIndex += currentIndex < itemStacks.size() - 1 ? 1 : 0;
+        }
+    }
+
+    private void drawItem(ItemStack stack, int x, int y) {
+        GlStateManager.enableRescaleNormal();
+        RenderHelper.enableGUIStandardItemLighting();
+        Optional.ofNullable(stack).ifPresent(s -> mc.getRenderItem().renderItemIntoGUI(stack, x + 1, y + 1));
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.disableRescaleNormal();
+    }
+
+    private void drawItems(int x, int y) {
+        if (!itemStacks.isEmpty()) {
+            if (currentIndex != 0) {
+                drawItem(itemStacks.get(currentIndex - 1), x - 20, y);
+            }
+            if (currentIndex != itemStacks.size() - 1) {
+                drawItem(itemStacks.get(currentIndex + 1), x + 20, y);
+            }
+            drawItem(itemStacks.get(currentIndex), x, y);
+        }
+    }
+
+    private void drawFrame() {
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.disableLighting();
+        GlStateManager.disableDepth();
+        GlStateManager.disableAlpha();
+        drawRect(x, y, x + WIDTH, y + HEIGHT, COLOR_BACKGROUND);
+        drawRect(x + 1, y + 1, x + WIDTH - 1, y + HEIGHT - 1, COLOR_FOREGROUND);
+        GlStateManager.enableAlpha();
+        GlStateManager.enableDepth();
+        GlStateManager.enableLighting();
+        GlStateManager.disableRescaleNormal();
+    }
+
+    private boolean isMouseOver() {
+        int mouseX = Mouse.getEventX() * parent.width / mc.displayWidth;
+        int mouseY = parent.height - Mouse.getEventY() * parent.height / mc.displayHeight - 1;
+        return mouseX >= x && mouseX <= x + WIDTH && mouseY >= y && mouseY <= y + HEIGHT;
+    }
+
+    public ItemStack getItemStack() {
+        if (itemStacks.isEmpty()) {
+            return null;
+        }
+        return itemStacks.get(currentIndex);
+    }
 }
